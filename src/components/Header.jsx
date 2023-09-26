a5b1688ceb43f1b45709331be72aefd508ffa352
MountEl002 <ojuokelias@gmail.com>
2023-09-26 23:10:44 +0300

    Project code base

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,24 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../ThemeContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faToggleOn, faToggleOff } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   const { isDarkMode, toggleTheme } = useContext(ThemeContext);
 
   return (
     <header className={`${isDarkMode ? "header header--dark" : "header"} `}>
+      <span>{isDarkMode ? "Light Mode" : "Dark Mode"}</span>
       <button className="header__button" onClick={toggleTheme}>
-        {isDarkMode ? "Light Mode" : "Dark Mode"}
+        {isDarkMode ? (
+          <span>
+            <FontAwesomeIcon icon={faToggleOff} style={{ height: "30px" }} />
+          </span>
+        ) : (
+          <span>
+            <FontAwesomeIcon icon={faToggleOn} style={{ height: "30px" }} />
+          </span>
+        )}
       </button>
     </header>
   );
